8c672b6988423ecc6d540bf6ad0e690beb870515
Umut Altan <ualtn8@gmail.com>
2024-01-08 00:10:16 +0300

    iletisim sayfasi yapildi

--- js/script.js
+++ js/script.js
@@ -30,4 +30,32 @@ function scrollToTop() {
     }
 }
 
+// Sayfa yüklendiğinde rastgele bir kod oluştur
+            document.addEventListener('DOMContentLoaded', function () {
+                generateVerificationCode();
+            });
 
+            function generateVerificationCode() {
+                // Rastgele 5 basamaklı bir sayı oluştur
+                var randomCode = Math.floor(10000 + Math.random() * 90000);
+
+                // Sayıyı formdaki alanlara güncelleyerek göster
+                document.getElementById('verificationCode1').value = randomCode;
+                document.getElementById('verificationCode2').value = "";
+            }
+
+            function submitForm() {
+                // Kullanıcının girdiği kodları al
+                var userCode1 = document.getElementById('verificationCode1').value;
+                var userCode2 = document.getElementById('verificationCode2').value;
+
+                // Kodları karşılaştır
+                if (userCode1 == userCode2) {
+                    alert('Mesajınız başarıyla gönderildi!');
+                } else {
+                    alert('Hatalı doğrulama kodu! Lütfen tekrar deneyin.');
+                }
+
+                // Yeni bir doğrulama kodu oluştur
+                generateVerificationCode();
+            }
